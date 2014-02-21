ffb61bd8a85ec174ceaf9ffb93de0508346d010d
Robin Deits <robin.deits@gmail.com>
2014-02-21 13:55:10 -0500

    faster algorithms, limited number of sides, and pruning every step

--- convex_corners.m
+++ convex_corners.m
@@ -1,6 +1,7 @@
 function mask = convex_corners(grid)
 
 grid = logical(grid);
+max_num_sides = 25;
 
 initial_offset = [0.5, 0.5, -0.5, -0.5;
                  -0.5, 0.5, 0.5, -0.5];
@@ -40,17 +41,17 @@ while true
     for i = 1:size(neighbors, 2)
       new_corners = [corners, neighbors(:,i)];
       new_corners = new_corners(:,convhull(new_corners(1,:), new_corners(2,:)));
-      hash = num2str(new_corners(:,1:end-1)');
+      if size(new_corners, 2) >= max_num_sides + 1
+        continue
+      end
+      hash = sprintf('%f', new_corners(:,1:end-1));
       if ~isKey(explored_set, hash)
-%       plot(new_corners(1,:), new_corners(2,:))
-%       xlim([0, size(grid,1)+1])
-%       ylim([0, size(grid,2)+1])
-%       drawnow()
-        if any(inpolygon(black_squares(1,:), black_squares(2,:), new_corners(1,:), new_corners(2,:)))
+        [A, b] = poly2lincon(new_corners(1,:), new_corners(2,:),true);
+        if any(all(bsxfun(@minus, A * black_squares, b) <= 0))
           continue
         end
         explored_set(hash) = true;
-        new_active_set(hash) = new_corners(:,1:end-1); % sigh :(
+        new_active_set(hash) = new_corners(:,1:end-1);
       end
     end
   end
@@ -58,19 +59,30 @@ while true
     break
   end
   active_set = new_active_set.values();
+  areas = zeros(1,length(active_set));
+  for j = 1:length(active_set)
+    corners = active_set{j};
+    corners = [corners, corners(:,1)];
+    [A, b] = poly2lincon(new_corners(1,:), new_corners(2,:),true);
+    areas(j) = sum(all(bsxfun(@minus, A * white_squares, b) <= 0));
+  end
+  [~, ndx] = max(areas);
+  active_set = active_set(ndx);
+
 end
 
-areas = zeros(1,length(active_set));
-for j = 1:length(active_set)
-  corners = active_set{j};
-  xv = [corners(1,:), corners(1,1)];
-  yv = [corners(2,:), corners(2,1)];
-  IN = inpolygon(white_squares(1,:), white_squares(2,:), xv, yv);
-  areas(j) = sum(IN);
-end
-
-[~, ndx] = max(areas);
-corners = active_set{ndx};
+% areas = zeros(1,length(active_set));
+% for j = 1:length(active_set)
+%   corners = active_set{j};
+%   xv = [corners(1,:), corners(1,1)];
+%   yv = [corners(2,:), corners(2,1)];
+%   IN = inpolygon(white_squares(1,:), white_squares(2,:), xv, yv);
+%   areas(j) = sum(IN);
+% end
+% 
+% [~, ndx] = max(areas);
+% corners = active_set{ndx};
+corners = active_set{1};
 xv = [corners(1,:), corners(1,1)];
 yv = [corners(2,:), corners(2,1)];
 [R, C] = meshgrid(1:size(grid,1), 1:size(grid,2));
